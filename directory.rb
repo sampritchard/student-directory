99ce9a27079a671e9baf52ba065b2e28fe43944d
Sam Pritchard <sampritchard@msn.com>
2017-04-26 11:43:22 +0100

    (7) updated with TO-DO and code tidied

--- directory.rb
+++ directory.rb
@@ -1,30 +1,31 @@
+#TO DO-
+#Check for spelling errors
+#create a default value if no month entered
+
 #all the students put into an array
-#changed the students and put them into a hash so easier to read when called
-def input_students
+def input_students #input_students method
   puts "Please enter the names of the students"
   puts "To finish, just hit the return twice"
   #create an empty array
   students = []
-  #cohort = ["January", "February", "March", "April", "May", "June",
-  #"July", "August", "September", "October", "November", "December" ]
+  #this will be used to check for spelling
+  cohort_month = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"]
   #gets the first name
   name = gets.chomp
+  #gets the cohort they will be joining
   puts "And the cohort this student will be joining?"
-  cohort = gets.chomp.intern
+  #intern converts the cohort name to a symbol rather than a string
+  cohort = gets.chomp.capitalize.intern
+
   #while the name is NOT empty, repeat this code
   while !name.empty? && !cohort.empty? do
     students << {name: name, cohort: cohort}
       puts "Now we have #{students.count} students"
     name = gets.chomp
       puts "And cohort?"
-    cohort = gets.chomp.intern #intern converts the cohort string to a symbol
-  #  students << {name: name, cohort: :november}
-  #  puts "Now we have #{students.count} students"
-  #  name = gets.chomp
-  #  puts "And cohort?"
-  #  cohort = gets.chomp
+    cohort = gets.chomp.capitalize.intern #intern converts the cohort string to a symbol
   end
-  #return the array of students
   students
 end
 
@@ -54,7 +55,6 @@ def print(students)
     puts "#{index+1} #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
-
 #finally, we print the total number of students
 #student.count counts the number of elements in the student array
 def print_footer(students)
