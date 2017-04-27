9a9a0efab4ca63f99bfa8e31ac2ebc2bdbb3606a
Sam Pritchard <sampritchard@msn.com>
2017-04-27 09:14:49 +0100

    added File.open

--- directory.rb
+++ directory.rb
@@ -1,5 +1,17 @@
 @students = []
 
+def save_students
+  #open the file for writing
+  file = File.open("students.csv", "w")
+  #iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student [:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+file.close
+end
+
 def interactive_menu
   loop do
     print_menu
